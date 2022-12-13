fce9c10add43a60fe1853698fce70ae7794fe1d6
czp <1527474029@qq.com>
2022-12-13 10:46:50 +0800

    init

--- api_server/app.js
+++ api_server/app.js
@@ -1,46 +1,46 @@
-const express = require("express");
-const app = express();
-const jwt = require("jsonwebtoken");
-const expressJWT = require("express-jwt");
-require("./utils/index")(app);
-const path = require("path");
-const multer = require("multer");
-const config = require("./config");
+const express = require("express")
+const app = express()
+const jwt = require("jsonwebtoken")
+// const expressJWT = require("express-jwt");
+require("./utils/index")(app)
+const path = require("path")
+const multer = require("multer")
+const config = require("./config")
 
-app.use(
-  expressJWT({
-    secret: config.jwtSecretKey,
-  }).unless({
-    path: [/^\/api\//],
-  })
-);
+// app.use(
+//   expressJWT({
+//     secret: config.jwtSecretKey,
+//   }).unless({
+//     path: [/^\/api\//],
+//   })
+// );
 
 app.listen(8088, function () {
-  console.log("api server running at http://10.2.13.142");
-});
+  console.log("api server running at http://10.2.13.113")
+})
 
-app.use("/files", express.static(path.join(__dirname + "../../files")));
+app.use("/files", express.static(path.join(__dirname + "../../files")))
 // 写文件功能
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "files");
+    cb(null, "files")
   },
   filename: function (req, file, cb) {
     // console.log(file);
-    const type = file.mimetype.match(/(?<=\/)[a-zA-Z0-9]+/);
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix + `.${type}`);
+    const type = file.mimetype.match(/(?<=\/)[a-zA-Z0-9]+/)
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9)
+    cb(null, file.fieldname + "-" + uniqueSuffix + `.${type}`)
   },
-});
+})
 const upload = multer({
   storage: storage,
-}).single("file");
+}).single("file")
 
 const uploadFile = async (req, res) => {
   upload(req, res, function (err) {
     // console.log(req.file);
-    res.send(req.file);
-  });
-};
+    res.send(req.file)
+  })
+}
 
-app.post("/uploadFile", uploadFile);
+app.post("/uploadFile", uploadFile)
